3f6c2b9ca6407f6515187fefbe7dc21dfb060de6
Serguei Sayfulin <serguei.sayfulin@gmail.com>
2011-02-01 13:06:00 +0100

    Add : VisuServeur : class ObselType.

---
+++ VisuServeur/visu/src/com/lyon2/utils/ObselType.java
@@ -0,0 +1,52 @@
+package com.lyon2.utils;
+
+public class ObselType {
+	
+	// type the obsels
+	public static final String RETRO_ROOM_LOAD_RETROSPECTED_SESSION = "RetroLoadRetrospectedSessionEvent";
+	public static final String RETRO_ROOM_EXIT_RETROSPECTED_SESSION = "RetroExitRetrospectedSessionEvent";
+	public static final String RETRO_EXPAND_TRACE_LINE = "RetroExpandTraceLineEvent";
+	public static final String RETRO_MINIMIZE_TRACE_LINE = "RetroMinimizeTraceLineEvent";
+	public static final String RETRO_ADD_OBSEL_TYPE_TO_LINE = "RetroAddObselTypeToLineEvent";
+	public static final String RETRO_DELETE_OBSEL_TYPE_FROM_LINE = "RetroDeleteObselTypeFromLineEvent";
+	public static final String DELETE_TEXT_COMMENT = "DeleteTextComment";	
+	public static final String RETRO_DELETE_COMMENT_EVENT = "RetroDeleteCommentEvent";	
+	public static final String RETRO_SAVE_COMMENT_EVENT = "RetroSaveCommentEvent";	
+
+	// prefixes
+	public static final String PREFICS_RETRO_ROOM = "Retro";
+	public static final String PREFICS_PARAM_OBSEL = "has";
+	// name the parametres of the obsel
+	public static final String SYNC_ROOM_TRACE_ID = "syncRoomTraceId";
+	public static final String PARENT_TRACE_ID = "parentTrace";
+	
+	public static final String CAUSE = "cause";
+	public static final String UNKNOWN = "UNKNOWN";
+	public static final String LOAD_ANOTHER_SESSION = "LOAD_ANOTHER_SESSION";
+	public static final String VIDEO_TIME = "videoTime";
+	public static final String OBSEL_ID = "obselId";
+	public static final String TOOL_TIP_VALUE = "tooltipValue";
+	public static final String SESSION_ID = "sessionId";
+	public static final String SESSION_TITLE = "sessionTitle";
+	public static final String TRACE_SUBJECT_ID = "traceSubjectId";
+	public static final String USER_NAME = "userName";
+	public static final String USER_AVATAR = "userAvatar";
+	public static final String OBSEL_TYPE_NAME = "obselTypeName";
+	public static final String UI_WIDGET = "uiWidget";
+	public static final String MARKER = "MARKER";
+	public static final String INSTRUCTIONS = "INSTRUCTIONS";
+	public static final String KEYWORDS = "KEYWORDS";
+	public static final String MESSAGE = "MESSAGE";
+	public static final String DOCUMENT = "DOCUMENT";
+	public static final String PLUS_MINUS_BUTTON = "PLUS_MINUS_BUTTON";
+	public static final String CHEKBOX = "CHEKBOX";
+	public static final String COMMENT_VALUE = "commentValue";
+	public static final String COMMENT_ID = "commentId";
+	public static final String COMMENT_BEGIN_DATE = "commentBeginDate";
+	public static final String COMMENT_END_DATE = "commentEndDate";
+	public static final String COMMENT_EDIT_TYPE = "editType";
+
+	
+	public static final String PARAM_SYNC_ROOM_TRACE_ID = "syncRoomTraceId";
+	
+}
