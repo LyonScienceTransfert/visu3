4782ade7041b6fed5404dd9c4dbc8f8a7ed73545
Serguei Sayfulin <serguei.sayfulin@gmail.com>
2011-04-11 10:44:26 +0200

    Add :VisuServeur, classes UserStatus, SessionStatus.

---
+++ VisuServeur/visu/src/com/lyon2/utils/SessionStatus.java
@@ -0,0 +1,9 @@
+package com.lyon2.utils;
+
+public class SessionStatus {
+	
+	public static final int RECORDING = 3;
+	public static final int PAUSE     = 2;
+	public static final int CLOSE     = 1;
+	public static final int OPEN      = 0;
+}
