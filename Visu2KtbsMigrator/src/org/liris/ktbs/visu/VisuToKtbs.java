f1a4c1a5186824f96ba4f8254352c169d7b29cd3
dcram <damien.cram@gmail.com>
2011-05-04 18:06:06 +0200

    Importation du projet Visu2ToKtbsMigrator

---
+++ Visu2KtbsMigrator/src/org/liris/ktbs/visu/VisuToKtbs.java
@@ -0,0 +1,227 @@
+package org.liris.ktbs.visu;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.Reader;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+import java.util.TimeZone;
+import java.util.TreeSet;
+
+import org.liris.ktbs.client.Ktbs;
+import org.liris.ktbs.client.KtbsConstants;
+import org.liris.ktbs.client.KtbsRootClient;
+import org.liris.ktbs.domain.interfaces.IBase;
+import org.liris.ktbs.domain.interfaces.ITraceModel;
+import org.liris.ktbs.service.MultiUserRootProvider;
+import org.liris.ktbs.service.ResourceService;
+import org.liris.ktbs.utils.KtbsUtils;
+import org.liris.ktbs.visu.vo.ObselVO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.ibatis.common.resources.Resources;
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.ibatis.sqlmap.client.SqlMapClientBuilder;
+
+
+public class VisuToKtbs {
+
+	private static Logger logger = LoggerFactory.getLogger(VisuToKtbs.class);
+
+	private String rootUri;
+	private String visuUserName;
+	private String visuPasswd;
+	private String visuSharedBaseName;
+	private String retroRoomModelLocalName;
+	private String visuModelLocalName;
+
+	private MultiUserRootProvider rootProvider;
+
+	private SqlMapClient sqlMap;
+	
+	public VisuToKtbs(SqlMapClient sqlMap, String rootUri, String userName, String userPasswd,
+			String shareBase, String retroRoomModelLocalName,
+			String visuModelLocalName) {
+		
+		super();
+		this.sqlMap = sqlMap;
+		this.rootUri = rootUri;
+		this.visuUserName = userName;
+		this.visuPasswd = userPasswd;
+		this.visuSharedBaseName = shareBase;
+		this.retroRoomModelLocalName = retroRoomModelLocalName;
+		this.visuModelLocalName = visuModelLocalName;
+
+		this.rootProvider = Ktbs.getMultiUserRestRootProvider();
+	}
+
+	public static void main(String[] args) throws IOException, SQLException {
+
+		Reader reader = Resources.getResourceAsReader("ibatis/sqlMapConfig.xml");
+		SqlMapClient sqlMap = SqlMapClientBuilder.buildSqlMapClient(reader);
+
+		Properties properties = new Properties();
+		properties.load(new FileInputStream("visu2ktbs.properties"));
+		logger.info("Migration config:\n {}", properties.toString());
+
+		VisuToKtbs visuToKtbs = new VisuToKtbs(
+				sqlMap,
+				properties.getProperty("ktbs.root.uri"),
+				properties.getProperty("ktbs.root.username"),
+				properties.getProperty("ktbs.root.passwd"),
+				properties.getProperty("ktbs.shared.base"),
+				properties.getProperty("ktbs.model.retroroom"),
+				properties.getProperty("ktbs.model.visu"));
+
+		visuToKtbs.doMigration();
+	}
+
+	private int totalObselNb = -1;
+	private int obselCnt = 0;
+	
+	@SuppressWarnings("unchecked")
+	private void createTrace(String traceName) throws SQLException {
+		List<ObselVO> obsels = (List<ObselVO>)sqlMap.queryForList("obsel.getObselsInTrace", "%" + traceName + "%");
+		
+		Integer userId = VisuToKtbsUtils.parseUserId(traceName);
+		KtbsRootClient client = rootProvider.getClient(VisuToKtbsUtils.makeKtbsUserId(userId));
+		ResourceService service = client.getResourceService();
+
+		String username = VisuToKtbsUtils.makeKtbsUserId(VisuToKtbsUtils.parseUserId(traceName));
+
+		Calendar originCal = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+		originCal.setTimeInMillis(VisuToKtbsUtils.parseTraceOrigin(traceName).getTime());
+		String origin = KtbsConstants.XSD_DATETIME_FORMAT.format(originCal.getTime());
+
+		logger.info("{}% - Creating the stored trace {}", new Object[]{(obselCnt*100)/totalObselNb, traceName});
+		String storedTraceUri = service.newStoredTrace(
+				username, 
+				traceName,
+				getVisuTraceModelURI(service), 
+				origin, 
+				username);
+
+		if(storedTraceUri == null) {
+			logger.error("The KTBS failed to create the trace {}", traceName);
+			return;
+		}
+
+		List<ObselVO> orderedByendDTObsels = new LinkedList<ObselVO>();
+		// parse all rdf first
+		for(ObselVO vo:obsels) {
+			try {
+				vo.parseRdf(getVisuTraceModelURI(service), KtbsUtils.resolveParentURI(storedTraceUri));
+				orderedByendDTObsels.add(vo);
+			} catch (Exception e) {
+				logger.error("Error creating the obsel {}. RDF: \n{}\nfixed:\n{}", new Object[]{vo.getId(), vo.getRdf(), vo.getFixedRdf()});
+				logger.error("",e);
+				return;
+			}
+		}
+		
+		Collections.sort(orderedByendDTObsels, new Comparator<ObselVO>() {
+			@Override
+			public int compare(ObselVO o1, ObselVO o2) {
+				try {
+					Date date1 = KtbsUtils.parseXsdDate(o1.getEndDT());
+					Date date2 = KtbsUtils.parseXsdDate(o2.getEndDT());
+					return date1.compareTo(date2);
+				} catch (ParseException e) {
+					logger.error("Failed to compare endDT dates", e);
+					return 0;
+				}
+			}
+		});
+		
+		for(ObselVO vo:orderedByendDTObsels) {
+			obselCnt++;
+
+				String obselURI = service.newObsel(
+						storedTraceUri, 
+						null, 
+						vo.getTypeUri(), 
+						vo.getBeginDT(), 
+						vo.getEndDT(), 
+						null, 
+						null, 
+						vo.getSubject(), 
+						vo.getAttributes());
+
+
+				if(obselURI == null) {
+					logger.error("The KTBS failed or rejected the creation of the obsel {}");
+					logger.error("KTBS response status (KTBS status: {}, HTTP status: {})", new Object[]{service.getLastResponse().getKtbsStatus(), service.getLastResponse().getHttpStatusCode()});
+					logger.error("KTBS message {}", new Object[]{service.getLastResponse().getServerMessage()});
+					logger.error("The KTBS failed or rejected the creation of the obsel {}. RDF: \n{}\nfixed:\n{}", new Object[]{vo.getId(), vo.getRdf(), vo.getFixedRdf()});
+				} 
+		}
+	}
+
+	private String lazyVisuTraceModelUri = null;
+	private String getVisuTraceModelURI(ResourceService service) {
+		if(lazyVisuTraceModelUri == null) {
+			IBase base = service.getBase(visuSharedBaseName);
+			ITraceModel tm = base.get(visuModelLocalName, ITraceModel.class);
+			lazyVisuTraceModelUri = tm.getUri();
+		}
+		return lazyVisuTraceModelUri;
+	}
+
+	private void doMigration() throws SQLException {
+		createSharedBaseAndModels();
+		createKtbsBases(sqlMap);
+		createTraces(sqlMap);
+	}
+
+	private void createSharedBaseAndModels() {
+		rootProvider.openClient(visuUserName, visuUserName);
+		ResourceService service = rootProvider.getClient(visuUserName).getResourceService();
+		String baseUri = service.newBase(visuSharedBaseName, visuUserName);
+		service.newTraceModel(baseUri, visuModelLocalName);
+	}
+
+	@SuppressWarnings("unchecked")
+	private void createKtbsBases(SqlMapClient sqlMap) throws SQLException {
+
+		List<String> traces = (List<String>)sqlMap.queryForList("obsel.getTraces");
+
+		Set<Integer> userIds = new TreeSet<Integer>();
+		for(String trace:traces) {
+			userIds.add(VisuToKtbsUtils.parseUserId(trace));
+		}
+
+		for(Integer userId:userIds) {
+			String userName = VisuToKtbsUtils.makeKtbsUserId(userId);
+			rootProvider.openClient(userName, userName);
+			KtbsRootClient client = rootProvider.getClient(userName);
+			String uri = client.getResourceService().newBase(userName, userName);
+			logger.info("Created ktbs:Base at {}", uri);
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	private void createTraces(SqlMapClient sqlMap) throws SQLException {
+		List<ObselVO> allObsels = (List<ObselVO>)sqlMap.queryForList("obsel.getObsels");
+		logger.info("Number of obsels in visu database: {}", allObsels.size());
+		totalObselNb = allObsels.size();
+		
+		List<String> allTraces = (List<String>)sqlMap.queryForList("obsel.getTraces");
+		logger.info("Number of traces in visu database: {}", allTraces.size());
+		
+		// Ensures that the traces are not processed in the same order each time
+		Collections.shuffle(allTraces);
+		for(String trace:allTraces) {
+			String traceName = trace.replaceAll("<","").replaceAll(">","");
+			createTrace(traceName);
+		}
+	}
+}
---
+++ Visu2KtbsMigrator/src/org/liris/ktbs/visu/VisuToKtbsUtils.java
@@ -0,0 +1,151 @@
+package org.liris.ktbs.visu;
+import java.io.StringReader;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TimeZone;
+import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.liris.ktbs.client.KtbsConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+
+
+public class VisuToKtbsUtils {
+
+	private static Logger logger = LoggerFactory.getLogger(VisuToKtbsUtils.class);
+
+	public static Pattern traceNamePattern = Pattern.compile("trace-(\\d{4})(\\d{2})(\\d{2})(\\d{2})(\\d{2})(\\d{2})-(\\d+)");
+
+	public static Date parseTraceOrigin(String traceName) {
+		Matcher matcher = getMatcher(traceName);
+
+		int year  = Integer.parseInt(matcher.group(1));
+		int month  = Integer.parseInt(matcher.group(2));
+		int day  = Integer.parseInt(matcher.group(3));
+		int hour  = Integer.parseInt(matcher.group(4));
+		int minute  = Integer.parseInt(matcher.group(5));
+		int second  = Integer.parseInt(matcher.group(6));
+
+		Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+		cal.set(Calendar.YEAR, year);
+		cal.set(Calendar.MONTH, month-1);
+		cal.set(Calendar.DAY_OF_MONTH, day);
+		cal.set(Calendar.HOUR, hour);
+		cal.set(Calendar.MINUTE, minute);
+		cal.set(Calendar.SECOND, second);
+
+		return cal.getTime();
+	}
+
+	private static Matcher getMatcher(String traceName) {
+		Matcher matcher = traceNamePattern.matcher(traceName);
+
+		if(!matcher.find()) 
+			logger.warn("Could not match the trace name {} to the pattern {}", traceName, traceNamePattern.toString());
+		return matcher;
+	}
+
+	public static Integer parseUserId(String traceName) {
+		Matcher matcher = getMatcher(traceName);
+
+		int userId  = Integer.parseInt(matcher.group(7));
+		return userId;
+	}
+
+	public static String makeKtbsUserId(Integer userId) {
+		return "user" + userId;
+	}
+
+
+	public static Model buildObselModel(String rdf) {
+		Model model = ModelFactory.createDefaultModel();
+		model.read(new StringReader(rdf), KtbsConstants.JENA_TURTLE, "");
+		return model;
+	}
+
+
+	private static Map<String, String> getReplacements(String traceModelUri, String userbaseUri) {
+		Map<String, String> replacements = new HashMap<String, String>();
+
+		replacements.put("<../visu/>", "<"+traceModelUri+">");
+		replacements.put("^\\. a", "[] a");
+		replacements.put("<(trace-\\d{4}\\d{2}\\d{2}\\d{2}\\d{2}\\d{2}-\\d+)>", "<"+userbaseUri+"$1/>");
+		replacements.put("http://liris.cnrs.fr/silex/2009/ktbs/", "http://liris.cnrs.fr/silex/2009/ktbs#");
+		
+		replacements.put("\"\\W*\"Vouvoyer 'ou 'tutoyer'\"", "\"'Vouvoyer 'ou 'tutoyer'\"");
+		replacements.put("\"\\W*\"famme'\"", "\"'famme'\"");
+		replacements.put("\" \"Introduction' à l'Art\"", "\" 'Introduction' à l'Art\"");
+		replacements.put("\" \"ce' problème\"", "\"'ce' problème\"");
+		replacements.put("\" \"un' problème", "\" 'un' problème");
+		replacements.put("\" \"la vie est belle' avec roberto benigni ?\"", "\" 'la vie est belle' avec roberto benigni ?\"");
+
+		return replacements;
+	}
+
+
+	public static String fixRdfString(String rdf, String traceModelUri,  String userbaseUri) {
+		String fixedRdf = rdf;
+
+		// escape strings in string-typed attributes
+		String stringAttributePattern = "(^:\\w+\\W+)\"((?:[^\n\r;\\\\\t\\\"]*(?:\r|\n|\t|\\\\|\\\"))+[^\n\\\\\t\r\\\"]*)\";$";
+		Pattern pattern = Pattern.compile(stringAttributePattern, Pattern.MULTILINE);
+		Matcher matcher = pattern.matcher(fixedRdf);
+		if(matcher.find() && matcher.groupCount() >= 2) {
+			String stringValue = matcher.group(2);
+			String stringValueEscaped = stringToTurtleString(stringValue);
+			
+			fixedRdf = matcher.replaceFirst(Matcher.quoteReplacement(matcher.group(1) + " \"" + stringValueEscaped + "\";"));
+		}		
+		
+		
+		// applies other replacements
+		for(Entry<String, String> entry:getReplacements(traceModelUri, userbaseUri).entrySet()) {
+			pattern = Pattern.compile(entry.getKey(), Pattern.MULTILINE);
+			matcher = pattern.matcher(fixedRdf);
+			fixedRdf = matcher.replaceFirst(entry.getValue());
+		}
+
+		return fixedRdf;
+	}
+
+	public static String stringToTurtleString(String string) {
+		StringBuffer sb = new StringBuffer();
+		int len = string.length();
+		char c;
+
+		for (int i = 0; i < len; i++) {
+			c = string.charAt(i);
+			if (c == '\\') {
+//				sb.append('\\');
+//				sb.append('\\');
+				sb.append('_');
+			} else if (c == '\r') {
+//				sb.append('\\');
+//				sb.append('r');
+				sb.append('_');
+			} else if (c == '"') {
+//				sb.append('\\');
+//				sb.append('"');
+				sb.append('\'');
+			} else if (c == '\t') {
+//				sb.append('\\');
+//				sb.append('t');
+				sb.append(' ');
+			} else if (c == '\n') {
+//				sb.append('\\');
+//				sb.append('n');
+				sb.append('_');
+			} else
+				sb.append(c);
+		}
+
+		return sb.toString();
+	}
+}
