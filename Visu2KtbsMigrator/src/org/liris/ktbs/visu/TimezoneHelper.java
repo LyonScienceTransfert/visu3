93d67f3d254e52a242d9a71520764972bce8d7ef
dcram <damien.cram@gmail.com>
2011-05-05 12:40:40 +0200

    logback again

---
+++ Visu2KtbsMigrator/src/org/liris/ktbs/visu/TimezoneHelper.java
@@ -0,0 +1,13 @@
+package org.liris.ktbs.visu;
+
+import java.util.TimeZone;
+
+public class TimezoneHelper {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(TimeZone.getDefault());
+	}
+}
