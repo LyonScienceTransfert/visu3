1c43ba2bd35f975551e2824a094dc67d82d4d4de
dcram <damien.cram@gmail.com>
2011-05-27 12:00:17 +0200

    Intégration de ktbs4j version 2.3.x

--- VisuServeur/visu/src/com/lyon2/visu/ktbs/KtbsApplicationHelper.java
+++ VisuServeur/visu/src/com/lyon2/visu/ktbs/KtbsApplicationHelper.java
@@ -1,6 +1,7 @@
 package com.lyon2.visu.ktbs;
 
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -8,91 +9,100 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.ConcurrentHashMap;
 
-import org.liris.ktbs.client.KtbsRootClient;
+import org.liris.ktbs.client.ClientFactory;
+import org.liris.ktbs.client.KtbsClient;
 import org.liris.ktbs.domain.interfaces.IBase;
 import org.liris.ktbs.domain.interfaces.IStoredTrace;
 import org.liris.ktbs.domain.interfaces.ITraceModel;
-import org.liris.ktbs.service.MultiUserRootProvider;
 import org.liris.ktbs.service.ResourceService;
 import org.liris.ktbs.service.StoredTraceService;
 import org.liris.ktbs.service.impl.ObselBuilder;
-import org.liris.ktbs.service.impl.RootAwareService;
 import org.liris.ktbs.utils.KtbsUtils;
 import org.red5.logging.Red5LoggerFactory;
 import org.slf4j.Logger;
-
-import com.ibatis.sqlmap.client.SqlMapClient;
-import com.lyon2.visu.domain.model.User;
 
 public class KtbsApplicationHelper {
 
 	private static Logger log = Red5LoggerFactory.getLogger(KtbsApplicationHelper.class, "visu2");
 
 	// injected by Spring
-	private MultiUserRootProvider rootProvider;
-	public void setRootProvider(MultiUserRootProvider rootProvider) {
-		this.rootProvider = rootProvider;
+	private ClientFactory clientFactory;
+	public void setClientFactory(ClientFactory clientFactory) {
+		this.clientFactory = clientFactory;
 	}
 
 	// injected by Spring
-	private SqlMapClient sqlMapClient;
-	public void setSqlMapClient(SqlMapClient sqlMapClient) {
-		this.sqlMapClient = sqlMapClient;
+	private String rootUri;
+	public void setRooUri(String rooUri) {
+		this.rootUri = rooUri;
+	}
+
+
+	// injected by Spring
+	private String sharedUsername;
+	public void setSharedUsername(String sharedUsername) {
+		this.sharedUsername = sharedUsername;
 	}
 
 	// injected by Spring
-	private String username;
-	public void setUsername(String username) {
-		this.username = username;
+	private String sharedPassword;
+	public void setSharedPassword(String sharedPassword) {
+		this.sharedPassword = sharedPassword;
 	}
-	
-	// injected by Spring
-	private String passwd;
-	public void setPasswd(String passwd) {
-		this.passwd = passwd;
-	}
-	
+
 	// injected by Spring
 	private String retroRoomTraceModelName;
 	public void setRetroRoomTraceModelName(String retroRoomTraceModelName) {
 		this.retroRoomTraceModelName = retroRoomTraceModelName;
 	}
-	
+
 	// injected by Spring
 	private String visuTraceModelName;
 	public void setVisuTraceModelName(String visuTraceModelName) {
 		this.visuTraceModelName = visuTraceModelName;
 	}
-	
+
 	// injected by Spring
 	private String sharedBaseName;
 	public void setSharedBaseName(String sharedBaseName) {
 		this.sharedBaseName = sharedBaseName;
 	}
-	
-	// the root client for the user "visu"
-	private KtbsRootClient visuUserRootClient;
 
 	// a flag, true if all KTBS resources have been created on / downloaded from the KTBS
 	// false otherwise
 	private boolean started = false;
-	
+
 	public boolean isStarted() {
 		return started;
 	}
 
-	private KtbsRootClient getVisuUserRootClient() {
-		if(visuUserRootClient == null) {
-			if(rootProvider.hasClient(username))
-				visuUserRootClient = rootProvider.getClient(username);
-			else {
-				if(rootProvider.openClient(username, passwd))
-					visuUserRootClient = rootProvider.getClient(username);
+	private Map<String, KtbsClient> ktbsClients = new HashMap<String, KtbsClient>();
+
+	private KtbsClient getUserKtbsClient(Integer subject) throws SQLException {
+		String ktbsUserName = makeKtbsUserName(subject);
+		String ktbsUserPassword = ktbsUserName;
+		KtbsClient ktbsClientClient = getKtbsClient(ktbsUserName, ktbsUserPassword);
+		return ktbsClientClient;
+	}
+
+	private KtbsClient getSharedKtbsClient() {
+		return getKtbsClient(sharedUsername, sharedPassword);
+	}
+
+	private KtbsClient getKtbsClient(String userName, String userPassword) {
+		if(ktbsClients.get(userName) == null) {
+			KtbsClient ktbsClient = clientFactory.createRestClient(rootUri, userName, userPassword);
+			ktbsClients.put(userName, ktbsClient);
+			String baseUri = ktbsClient.getResourceService().newBase(userName);
+
+			if(log.isDebugEnabled()) {
+				if(baseUri != null)
+					log.debug("The base {} has been created on KTBS", baseUri);
 				else
-					log.error("Could not start the Ktbs service. Impossible to create a root client for the user \"share\".");
+					log.debug("A base already exists on the KTBS for the user {}. No need to create one", userName);
 			}
 		}
-		return visuUserRootClient;
+		return ktbsClients.get(userName);
 	}
 
 	// invoked from Spring (destroy-method)
@@ -108,24 +118,24 @@ public class KtbsApplicationHelper {
 	public void init() {
 		log.info("Initializing the KTBS Client.");
 
-		// creates the KtbsRootClientObject
-		KtbsRootClient client = getVisuUserRootClient();
+		// creates the KtbsClientObject
+		KtbsClient client = getSharedKtbsClient();
 		String notInitializedMessage = "The KTBS client service could not be initialized";
 		if(client == null) {
 			log.warn(notInitializedMessage + " (root client is null)");
 			return;
 		}
 
-		log.info("The KTBS root is: " + getVisuUserRootClient().getRootUri());
+		log.info("The KTBS root is: " + getSharedKtbsClient().getRootUri());
 		ResourceService resourceService = client.getResourceService();
 
 		// creates the java object for the ktbs:Base "visuBase"
 		IBase visuBase = resourceService.getBase(sharedBaseName);
 		if(visuBase == null) {
 			log.info("The base \""+sharedBaseName+"\" does not exist. Creating it.");
-			log.debug("Root URI is DefaultResourceManager is: " + ((RootAwareService)resourceService).getRootUri());
-			
-			String visuBaseUri = resourceService.newBase(sharedBaseName, username);
+			log.debug("Root URI is DefaultResourceManager is: " + resourceService.getRootUri());
+
+			String visuBaseUri = resourceService.newBase(sharedBaseName);
 			if(visuBaseUri == null) {
 				log.error("Could not create a base for the user \"visu\".");
 			} else
@@ -153,12 +163,12 @@ public class KtbsApplicationHelper {
 		this.started = true;
 	}
 
-	private ITraceModel initializeTraceModel(IBase base, String modelLocalName, KtbsRootClient client) {
+	private ITraceModel initializeTraceModel(IBase base, String modelLocalName, KtbsClient client) {
 		ITraceModel model = base.get(modelLocalName, ITraceModel.class); 
 		if(model == null) {
 			log.info("The trace model \""+modelLocalName+"\" does not exist. Creating it.");
-			model = client.getResourceService().newTraceModel(base.getUri(), modelLocalName);
-			if(model == null) 
+			String modelUri = client.getResourceService().newTraceModel(base.getUri(), modelLocalName);
+			if(modelUri == null) 
 				log.error("The trace model \""+modelLocalName+"\" could not be created."); 
 			else {
 				log.info("Creating the content of the \"Retro room\" trace model. May take a few seconds...");
@@ -199,7 +209,7 @@ public class KtbsApplicationHelper {
 		} else
 			log.info("Sending the obsel to the KTBS: " + getObselSimpleString(subject, trace, typeObsel));
 
-		KtbsRootClient rootClient = getRootClient(subject);
+		KtbsClient rootClient = getUserKtbsClient(subject);
 		if(rootClient == null) {
 			log.warn("Could not get a root client for the user: " + subject);
 			return;
@@ -244,27 +254,6 @@ public class KtbsApplicationHelper {
 			log.info("The obsel " + uri + " has been created on the KTBS");
 
 	}
-	
-	private KtbsRootClient getRootClient(Integer subject) throws SQLException {
-		String ktbsUserName = makeKtbsUserName(subject);
-		KtbsRootClient rootClient = null;
-		if(rootProvider.hasClient(ktbsUserName)) {
-			log.debug("A root client already exists for the user " + subject + " (base name: "+ktbsUserName+")");
-			rootClient = rootProvider.getClient(ktbsUserName);
-		} else {
-			log.debug("A root client does not exist for the user " + subject + " (base name: "+ktbsUserName+")");
-			User user = (User) sqlMapClient.queryForObject("users.getUser", subject);
-			log.debug("Opening a root client for the user " + subject + " (username: "+ user.getFirstname() + " " + user.getLastname() +", mail: "+user.getMail()+", passwd: "+user.getPassword()+")");
-			if(rootProvider.openClient(ktbsUserName, user.getPassword())) {
-				log.debug("Root client opened");
-				rootClient = rootProvider.getClient(ktbsUserName);
-			} else {
-				log.warn("Opening failed");
-			}
-		}
-
-		return rootClient;
-	}
 
 	private IBase getBase(Integer userId, ResourceService resourceService) {
 		IBase base = baseCache.get(userId);
@@ -277,24 +266,11 @@ public class KtbsApplicationHelper {
 			// retrieve the base from the KTBS
 			base = resourceService.getBase(baseName);
 			if(base == null) {
-				log.debug("No base " + baseName + " exists on the KTBS server");
-				// create the base
-				log.info("Creating the base " + baseName + " on the KTBS server for the user " + userId);
-				
-				String userBaseUri = resourceService.newBase(
-						baseName,
-						Integer.toString(userId)
-				);
-				if(userBaseUri == null) 
-					log.error("Could not create the base " + baseName + " for the user ");
-				else {
-					log.debug("The base has been created");
-					base = resourceService.getBase(userBaseUri);
-				}
+				log.error("No base " + baseName + " exists on the KTBS server");
+				log.error("Ktbs response status: {}", resourceService.getLastResponse().getHttpStatusCode());
+				log.error("Ktbs response message: \n{}",  resourceService.getLastResponse().getServerMessage());
 			}
-		}
-
-		if(base != null)
+		} else
 			baseCache.put(userId, base);
 		return base;
 	}
@@ -310,20 +286,20 @@ public class KtbsApplicationHelper {
 			Integer userId, 
 			String traceLocalName, 
 			ResourceService resourceService) throws SQLException {
-		
+
 		String baseName = makeKtbsUserName(userId);
-		
+
 		IStoredTrace storedTrace = storedTraceCache.get(traceLocalName);
-		
+
 		if(storedTrace == null) {
 			log.debug("The trace " + traceLocalName + " is not in the cache");
-			
+
 			log.info("Retrieving the trace " + traceLocalName + " from the KTBS");
 			storedTrace = resourceService.getResource(baseName + "/" + traceLocalName, IStoredTrace.class);
-			
+
 			if(storedTrace == null) {
 				log.info("The trace " + traceLocalName + " could not be retrieved from the KTBS. Creating a new one.");
-				
+
 				String storedTraceUri = resourceService.newStoredTrace(
 						baseName, 
 						traceLocalName, 
